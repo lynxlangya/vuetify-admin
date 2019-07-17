08e04dcc60ebfd482b568bbbd1fa87c23cfc0169
MARS <marshal_max@163.com>
2019-07-17 14:08:47 +0800

    chore(package): vue.config.js and editorconfig add

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
 /**
  * @author MARS <marshal_max@163.com>
- * @package BundleAnalyzerPlugin 可视化打包
+ *
+ * @module BundleAnalyzerPlugin 可视化打包
  */
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
     configureWebpack: config => {
         if (process.env.NODE_ENV === 'production') {
